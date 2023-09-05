64a371b78ef11fa8ebfea5d90ef3a9b2707c6608
David Veríssimo Leite <davidd.verissimo@gmail.com>
2023-09-05 08:50:15 -0300

    projetoAgenda

---
+++ node/projetoagenda/frontend/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import './assets/css/style.css';
+
+
+// mongodb+srv://daviddverissimo:j8i6g9n6@cluster0.gobnlci.mongodb.net/?retryWrites=true&w=majority
