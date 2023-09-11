d7fc4ccd29a995a8016d14bcfda9027b2dc78b01
David Veríssimo Leite <davidd.verissimo@gmail.com>
2023-09-11 07:51:15 -0300

    ProjetoAgenda
    Projeto Agenda utilizando Express e MongoDB.

--- node/projetoagenda/frontend/main.js
+++ node/projetoagenda/frontend/main.js
@@ -1,6 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import './assets/css/style.css';
+// import './assets/css/style.css';
 
 
 // mongodb+srv://daviddverissimo:j8i6g9n6@cluster0.gobnlci.mongodb.net/?retryWrites=true&w=majority
--- node/projetoagenda/src/controllers/loginController.js
+++ node/projetoagenda/src/controllers/loginController.js
@@ -1,27 +1,62 @@
 const Login = require('../models/LoginModel');
 
-exports.index = (req,res) => {
-    res.render('login');
+exports.index = (req, res) => {
+    if (req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
-exports.register = async function(req, res) {
+exports.register = async function (req, res) {
     try {
         const login = new Login(req.body);
-        await login.register();    
-        if(login.errors.length > 0) {
+        await login.register();
+
+        if (login.errors.length > 0) {
             req.flash('errors', login.errors);
-            req.session.save(function(){
-              return res.redirect('back');
+            req.session.save(function () {
+                return res.redirect('/login/index');
             });
             return;
         }
-      
-        req.flash('success', 'Seu usuário foi criado com sucesso.');
-        req.session.save(function(){
-          return res.redirect('back');
+
+        req.flash('success', 'seu usuário foi criado com sucesso.');
+        req.session.save(function () {
+            return res.redirect('/login/index');
         });
-    } catch(e) {
+
+
+    } catch (e) {
         console.log(e);
-       return res.render('404');
+        return res.render('404');
     }
+};
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Voce entrou no sistema.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
 };
